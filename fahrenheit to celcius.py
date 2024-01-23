be8a48700a4272a27103f297d59959bd2fb53031
2203031260200 <157494138+2203031260200@users.noreply.github.com>
2024-01-23 18:51:29 +0530

    Add files via upload

---
+++ fahrenheit to celcius.py
@@ -0,0 +1,8 @@
+#f=(9/5)c+32 celcius fahernhit
+c= float (input("enter celcius value:"))
+f=(c*(9/5)+32)
+print("fahrenheit is :",f)
+#,celceius =(fahrenheit-32)x5/9 fahernheit to celcius
+f= float(input("enter value in fahernheit:"))
+c=((f-32)*5/9)
+print("celcius conversion is :",c)
