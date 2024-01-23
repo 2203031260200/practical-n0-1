d317db67bcfa51b6f50b42fed8ea94db46db8785
2203031260200 <157494138+2203031260200@users.noreply.github.com>
2024-01-23 23:58:28 +0530

    Update fahrenheit to celcius.py

--- fahrenheit to celcius.py
+++ fahrenheit to celcius.py
@@ -1,8 +1,9 @@
-#f=(9/5)c+32 celcius fahernhit
+fh=open("output1.txt",'w')
 c= float (input("enter celcius value:"))
 f=(c*(9/5)+32)
-print("fahrenheit is :",f)
+fh.write("fahrenheit is :"+str(f))
 #,celceius =(fahrenheit-32)x5/9 fahernheit to celcius
 f= float(input("enter value in fahernheit:"))
 c=((f-32)*5/9)
-print("celcius conversion is :",c)
+fh.write("celcius conversion is :"+str(c))
+fh.close
